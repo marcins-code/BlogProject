631513daad9d6fe88680912795d454425b86c251
marcins-code <mpaczkowski40@gmail.com>
2020-04-24 19:41:26 +0200

    Icons update

--- src/Form/CategoryFormType.php
+++ src/Form/CategoryFormType.php
@@ -7,6 +7,7 @@ use App\Repository\CategoryRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -28,21 +29,22 @@ class CategoryFormType extends AbstractType
     {
 
         $request = Request::createFromGlobals();
-
         $path = $request->getPathInfo();
         $pattern = '/^\/admin\/category\/edit\//';
-
         if (preg_match($pattern, $path)) {
             $cat_id = preg_replace($pattern, '', $path);
         } else {
 
             $cat_id = 0;
         }
+
         $builder
-            ->add('category')
+            ->add('category', TextType::class,[
+                'required'=>false,
+            ])
+            ->add('icon')
             ->add('description')
             ->add('isEnabled')
-
             ->add('parent', EntityType::class, [
                 'class' => Category::class,
                 'choices' => $this->repository->findOnlyMainCategories($cat_id),
