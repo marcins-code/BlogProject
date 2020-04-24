f2952b126893ea06fb8f471c0f84cea9329ffdf1
marcins-code <mpaczkowski40@gmail.com>
2020-04-24 09:47:56 +0200

    Login and registration

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -3,10 +3,12 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UsersRepository")
+ * @UniqueEntity(fields={"username"}, message="There is already an account with this username")
  */
 class Users implements UserInterface
 {
@@ -44,7 +46,7 @@ class Users implements UserInterface
     private $lastName;
 
     /**
-     * @ORM\Column(type="string", length=100)
+     * @ORM\Column(type="string", length=100, nullable=true)
      */
     private $email;
 
