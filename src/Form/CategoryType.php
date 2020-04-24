63eca1a22150e673eb06a69ba60ccddad8935da5
marcins-code <mpaczkowski40@gmail.com>
2020-04-24 10:36:34 +0200

    Add scss and js

---
+++ src/Form/CategoryType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategoryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('category')
+            ->add('slug')
+            ->add('description')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('createdBy')
+            ->add('isEnabled')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Category::class,
+        ]);
+    }
+}
