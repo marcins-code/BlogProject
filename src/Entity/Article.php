9c6dcea8012518f347d60dd65ec1e03e4c1ee850
marcins-code <mpaczkowski40@gmail.com>
2020-04-27 05:23:45 +0200

    Update Article.php

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -61,6 +61,11 @@ class Article
      */
     private $movies = [];
 
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $slug;
+
 
 
 
@@ -174,6 +179,18 @@ class Article
     public function setMovies(?array $movies): self
     {
         $this->movies = $movies;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
 
         return $this;
     }
